a893385c92f57e6d3c4ef4eb4745e6a87a2773cf
ahzongo <abdelhassane.zongo@gmail.com>
2022-01-21 14:04:40 +0100

    deploy router config

--- script.py
+++ script.py
@@ -1,22 +1,26 @@
 import json
 import re
+import subprocess
 
 def defineHeader(hostname):
-    Header = "!\n"*5 + "version 15.2\n" + "service timestamps debug datetime msec\n" + "service timestamps log datetime msec\n" + "!\n" + "hostname "+ hostname +"\n" + "!\n" + "boot-start-marker\n" +"boot-end-marker\n" + "!\n"*3 + "no aaa new-model\n" + "no ip icmp rate-limit unreachable\n" + "ip cef\n" + "!\n"*6 +"no ip domain lookup\n"+ "no ipv6 cef\n"+ "!\n"*2 +"mpls label protocol ldp \nmultilink bundle-name authenticated\n" +"!\n"*9 +"ip tcp synwait-time 5\n" + "!\n"*12
+    Header = "!\n"*9 + "version 15.2\n" + "service timestamps debug datetime msec\n" + "service timestamps log datetime msec\n" + "!\n" + "hostname "+ hostname +"\n" + "!\n" + "boot-start-marker\n" +"boot-end-marker\n" + "!\n"*3 + "no aaa new-model\n" + "no ip icmp rate-limit unreachable\n" + "ip cef\n" + "!\n"*6 +"no ip domain lookup\n"+ "no ipv6 cef\n"+ "!\n"*2 +"mpls label protocol ldp \nmultilink bundle-name authenticated\n" +"!\n"*9 +"ip tcp synwait-time 5\n" + "!\n"*12
     return Header
 
 def defineFooter():
-    Fin = "\n" + "ip forward-protocol nd\n"+"!\n"*2 +"no ip http server\n"+"no ip http secure-server\n"+"!\n"*4 +"control-plane\n"+"!\n"*2+"line con 0\n"+"exec-timeout 0 0\n"+"privilege level 15\n" + "logging synchronous\n" + "stopbits 1 \n" +"line aux 0\n" +"exec-timeout 0 0\n" +"privilege level 15\n" + "logging synchronous\n"+"stopbits 1\n"+"line vty 0 4\n"+"login\n" + "!\n" *2 +"end"
+    Fin = "\n" + "ip forward-protocol nd\n"+"!\n"*2 +"no ip http server\n"+"no ip http secure-server\n"+"!\n"*4 +"control-plane\n"+"!\n"*2+"line con 0\n"+" exec-timeout 0 0\n"+" privilege level 15\n" + " logging synchronous\n" + " stopbits 1 \n" +"line aux 0\n" +" exec-timeout 0 0\n" +" privilege level 15\n" + " logging synchronous\n"+" stopbits 1\n"+"line vty 0 4\n"+" login\n" + "!\n" *2 +"end"
     return Fin
 
 def defineInterfaceConfig(interface):
     ipConfig = ""
     if ( interface["config"] == 'no' ):
-        ipConfig = "interface " + interface["name"] +" \n no ip address \n shutdown \n duplex full \n!"
+        if (re.search("^Gigabit*", interface["name"] )):
+            ipConfig = "interface " + interface["name"] +" \n no ip address\n shutdown\n negotiation auto\n!"
+        else:
+            ipConfig = "interface " + interface["name"] +" \n no ip address\n shutdown\n duplex full\n!"
     elif (re.search("^Loopback*", interface["name"] )): 
         ipConfig = "interface "+ interface["name"] + "\n ip address " + interface["ip"] + " "+ interface["mask"] + "\n!"
     else: 
-        ipConfig = "interface "+ interface["name"] + "\n ip address " + interface["ip"] + " " + interface["mask"] + "\n"+" negotiation auto \n mpls ip \n!"
+        ipConfig = "interface "+ interface["name"] + "\n ip address " + interface["ip"] + " " + interface["mask"] + "\n"+" negotiation auto\n mpls ip\n!"
 
 
     return ipConfig
@@ -37,8 +41,9 @@ def defineOSPFConfig(router):
     " router-id " + router["router-id"]+" \n"
 
     for int in router["interfaces"] : 
-        networkInfo = getNetwork(int["ip"], int["mask"])
-        ospfConfig = ospfConfig + " network " +networkInfo[0]+" "+ networkInfo[1] +" \n"
+        if(int["config"] == "yes"):
+            networkInfo = getNetwork(int["ip"], int["mask"])
+            ospfConfig = ospfConfig + " network " +networkInfo[0]+" "+ networkInfo[1] +" area 0\n"
     
     ospfConfig = ospfConfig + "! \n"
 
@@ -57,7 +62,7 @@ def defineEdgeRouterConfig(router):
     config = defineHeader(router["name"])
     for int in router["interfaces"]:
         config = config + defineInterfaceConfig(int) + "\n"
-    
+    config = config + defineOSPFConfig(router)
     config = config + defineFooter()
 
     return config
@@ -67,17 +72,32 @@ def getTopology(file):
         data = json.load(f)
     return data
 
+def getConfigFile(hostname):
+    command = "grep -iRl \"^hostname "+hostname+"\" /home/abdel/GNS3/projects/test_script/project-files/dynamips/ | grep .cfg"
+    return str(subprocess.check_output(['bash','-c', command])).split("'")[1][:-2]
+
+
 def createCfgFile(name, config):
-    with open(name+'.cfg', 'w') as f:
+    with open(name, 'w') as f:
         f.write(config)
+
+def deployTopology(topology):
+    
+    for edge in topology["PE"]:
+        confFile = getConfigFile(edge["name"])
+        createCfgFile(confFile, defineEdgeRouterConfig(edge))
+        #print(confFile)
+    for core in topology["Core"]:
+        confFile = getConfigFile(core["name"])
+        createCfgFile(confFile, defineCoreRouterConfig(core))
+        #print(confFile)
 
 
 def main() :
     data = getTopology('data.json')
-    for core in data["Core"]:
-        createCfgFile(core["name"], defineCoreRouterConfig(core))
-    for core in data["PE"]:
-        createCfgFile(core["name"], defineEdgeRouterConfig(core))
+    deployTopology(data)
+    
+    #createCfgFile(getConfigFile("R1"), 'test2 \nhostname R1')
 
 
 if __name__ == "__main__" : 
