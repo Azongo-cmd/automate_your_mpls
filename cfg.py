4181b8a58656d8cdaa040de2f7e8ba4fa88c3bc2
Bella99b <maimouna-bella.diallo@insa-lyon.fr>
2022-02-10 23:20:17 +0100

    define vrf config

--- cfg.py
+++ cfg.py
@@ -2,4 +2,5 @@ AS = "100"
 CLIENT_COMUNITY = AS + ":1110"
 PEER_COMUNITY = AS + ":1120"
 PROVIDER_COMUNITY = AS + ":1130"
+RD = 10
 PROJECT_DIR = "/home/abdel/GNS3/projects/test_script/project-files/dynamips/"
--- main.py
+++ main.py
@@ -8,8 +8,10 @@ PROJECT_DIR = "/home/abdel/GNS3/projects/test_script/project-files/dynamips/"
 
 def main() :
     data = utils.getTopology('data.v2.json')
-    s.deployTopology(data, PROJECT_DIR)
+    #s.deployTopology(data, PROJECT_DIR)
     #print(s.defineRouterConfig(data,data["routers"][0]))
+    print(s.defineVRFConfig(data["routers"][0]))
+    print(s.defineVRFConfig(data["routers"][3]))
 
 
 if __name__ == "__main__": 
